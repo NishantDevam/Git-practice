05b0c9d8cc2901ca0e70574f0fd7907567b5844f
Astitva Singh <singhastitva99@gmail.com>
2020-07-19 18:12:57 +0530

    initial commit

---
+++ student.java
@@ -0,0 +1,10 @@
+package OOPs;
+
+public class student {
+       public String name;
+       public int age;
+       
+       public void getdetails()  {
+    	   System.out.println("My name is "+name+" and my age is "+age);
+       }
+}
